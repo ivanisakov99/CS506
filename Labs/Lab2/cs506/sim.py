b465c8395c4a32e7171f812dc6d211b8a5994b9f
ivanisakov99 <isakovi@bu.edu>
2022-02-07 23:17:34 -0500

    Update sim.py

--- Labs/Lab2/cs506/sim.py
+++ Labs/Lab2/cs506/sim.py
@@ -1,4 +1,4 @@
-# import numpy as np #???
+import numpy as np
 from cmath import sqrt
 
 def euclidean_dist(x, y):
@@ -21,6 +21,6 @@ def jaccard_dist(x, y):
 def cosine_sim(x, y):
     if len(x) == 0 or len(y) == 0 or len(x) != len(y):
         return 0
-    return sum([x[i] * y[i] for i in range(len(x))]) / (sqrt(sum([elem ** 2 for elem in x])) * sqrt(sum([elem ** 2 for elem in y])))
+    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
 
 # Feel free to add more
