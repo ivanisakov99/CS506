5b71fdb46489c330bdf67d329e68684ff28bacf0
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2022-02-08 10:50:26 -0500

    Add function descriptions

--- Labs/Lab2/cs506/sim.py
+++ Labs/Lab2/cs506/sim.py
@@ -1,24 +1,36 @@
 import numpy as np
 from cmath import sqrt
 
+
 def euclidean_dist(x, y):
+    """
+    """
     res = 0
     for i in range(len(x)):
         res += (x[i] - y[i])**2
     return res**(1/2)
 
+
 def manhattan_dist(x, y):
+    """
+    """
     return sum([abs(x[i] - y[i]) for i in range(len(x))])
 
+
 def jaccard_dist(x, y):
+    """
+    """
     if len(x) == 0 and len(y) == 0:
         return 0
-    
+
     intersection = len(set(x).intersection(y))
     union = (len(set(x)) + len(set(y))) - intersection
     return 1 - intersection / union
 
+
 def cosine_sim(x, y):
+    """
+    """
     if len(x) == 0 or len(y) == 0 or len(x) != len(y):
         return 0
     return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
